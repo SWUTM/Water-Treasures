6f2384681ae8e4ff518c60d7e5e12e5cfefecb7a
SWUTM <balcknutfdsfsddfsdsf@gmail.com>
2022-08-01 17:38:33 +0300

    1.16.5

---
+++ src/main/java/net/swutm/watertreasures/item/WaterWoodBowlItem.java
@@ -0,0 +1,35 @@
+
+package net.swutm.watertreasures.item;
+
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.InteractionResult;
+
+import net.swutm.watertreasures.procedures.WaterWoodBowlRightclickedOnBlockProcedure;
+
+public class WaterWoodBowlItem extends Item {
+	public WaterWoodBowlItem() {
+		super(new Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(64).rarity(Rarity.COMMON));
+	}
+
+	@Override
+	public UseAnim getUseAnimation(ItemStack itemstack) {
+		return UseAnim.EAT;
+	}
+
+	@Override
+	public int getUseDuration(ItemStack itemstack) {
+		return 0;
+	}
+
+	@Override
+	public InteractionResult useOn(UseOnContext context) {
+		InteractionResult retval = super.useOn(context);
+		WaterWoodBowlRightclickedOnBlockProcedure.execute(context.getPlayer());
+		return retval;
+	}
+}
---
+++ src/main/java/net/swutm/watertreasures/procedures/WaterWoodBowl2RightclickedOnBlockProcedure.java
@@ -0,0 +1,42 @@
+package net.swutm.watertreasures.procedures;
+
+import net.minecraftforge.items.ItemHandlerHelper;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
+
+import net.swutm.watertreasures.init.WaterTreasuresModItems;
+
+public class WaterWoodBowl2RightclickedOnBlockProcedure {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)
+				.getItem() == WaterTreasuresModItems.WATER_WOOD_BOWL_2.get()) {
+			if (entity instanceof Player _player) {
+				ItemStack _stktoremove = new ItemStack(WaterTreasuresModItems.WATER_WOOD_BOWL_2.get());
+				_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1,
+						_player.inventoryMenu.getCraftSlots());
+			}
+			if (entity instanceof Player _player) {
+				ItemStack _setstack = new ItemStack(Items.BOWL);
+				_setstack.setCount(1);
+				ItemHandlerHelper.giveItemToPlayer(_player, _setstack);
+			}
+			if (world instanceof ServerLevel _level)
+				_level.getServer().getCommands().performCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4,
+						"", new TextComponent(""), _level.getServer(), null).withSuppressedOutput(),
+						"/loot spawn ~ ~ ~ loot morenugget:blocks/water_bowl");
+		}
+	}
+}
